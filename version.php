a2268ce03627fad7987355531e55f64c20fed6b3
Server Dev <server@saviodev.utb.edu.co>
2025-10-02 17:06:41 +0000

    chore: bump version to 1.9.3

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin->version = 2025093002;
+$plugin->version = 2025093003;
 $plugin->requires = 2013050100;
 $plugin->component = 'block_student_path'; // Full name of the plugin (used for diagnostics)
-$plugin->release   = '1.9.2';
+$plugin->release   = '1.9.3';

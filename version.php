2c4931e1213a285d123db96be86c68aa08412a67
Isaac David Sánchez Sánchez <158796340+IsaacDev2024@users.noreply.github.com>
2026-01-18 12:57:33 -0500

    Update version to 2.0.2 in version.php

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Student Path Block Major Update
- * Version 2.0.1 - Production Ready
+ * Version 2.0.2 - Production Ready
  *
  * @package    block_student_path
  * @copyright  2026 Jairo Serrano, Yuranis Henriquez, Isaac Sanchez, Santiago Orejuela, Maria Valentina
@@ -10,11 +10,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026011800; // YYYYMMDDXX (year, month, day, 2-digit version number).
+$plugin->version = 2026011801; // YYYYMMDDXX (year, month, day, 2-digit version number).
 $plugin->requires = 2022041900; // Moodle 4.0+
 $plugin->component = 'block_student_path';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.0.1';
+$plugin->release = '2.0.2';
 
 $plugin->dependencies = [
     'block_chaside' => 2026010800,
